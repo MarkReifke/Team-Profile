2fdb70d14ac37df1a2efca424cd93d0cd54da67b
Mark Reifke <mark.reifke@gmail.com>
2021-03-23 11:03:47 -0700

    index edit

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+const inquirer = require("inquirer");
 const Manager = require("./lib/Manager");
+const prompts = require("./src/prompts");
+
+const employees = [];
+
+inquirer
+    .prompt([
+        {
+            name:"employee",
+            message: "What employee would you like to add?",
+            type:"list",
+            choices: ["Engineer", "Intern", "Finished"],
+        },
+    ])
+    .then(function (employee) {
+        console.log(response);
+        switch(employee){
+            case "Engineer":
+            break;
+            case "Intern":
+            break;
+            case "Finished":
+            break;
+        }
+    });    
