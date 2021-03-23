a3f7564da880ba72aeb867a2cdf5e45315c0de3f
Mark Reifke <mark.reifke@gmail.com>
2021-03-23 13:29:22 -0700

    made chages and added html and css

--- index.js
+++ index.js
@@ -123,24 +123,3 @@ function addIntern(){
     })
 }
 
-
-inquirer
-    .prompt([
-        {
-            name:"employee",
-            message: "What employee would you like to add?",
-            type:"list",
-            choices: ["Engineer", "Intern", "Finished"],
-        },
-    ])
-    .then(function (employee) {
-        console.log(response);
-        switch(employee){
-            case "Engineer":
-            break;
-            case "Intern":
-            break;
-            case "Finished":
-            break;
-        }
-    });    
